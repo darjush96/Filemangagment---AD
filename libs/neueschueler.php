85fa4330e7a64c13bfa3385e2373f3e892b1270c
Darjush <darjush96@hotmail.de>
2017-08-07 10:59:43 +0200

    Working things

--- libs/neueschueler.php
+++ libs/neueschueler.php
@@ -4,7 +4,12 @@ function neueschuelerabfrage(){
   include_once("libs/csvspaltennamen.php");
   $link = dbconn();
   selectdb($link, 'Adexport');
-  $sql    = "SELECT * FROM Export2 WHERE NOT EXISTS(SELECT NULL FROM Export1 WHERE Export1.verg_id = Export2.verg_id)AND Export2.benutzerstatus = 'Ja'";
+  $sql    = "SELECT DISTINCT *
+  FROM `export2`
+  LEFT JOIN `export1` ON export1.`name` = benutzername
+  WHERE `benutzerstatus` = '1'
+  AND `User Logon Name` IS NULL
+  AND `regelklasse` LIKE '%17%'";
   //
 //$sql    = "SELECT * FROM Export2 WHERE NOT EXISTS(SELECT NULL FROM Export1 WHERE Export1.verg_id = Export2.verg_id)"
     $resu = mysql_query($sql);
@@ -22,7 +27,7 @@ function neueschuelerabfrage(){
     while ($row = mysql_fetch_array($resu))
     {
       //$row["benutzerstatus"]
-      $line = array("","",$row["name"],"",$row["vorname"],$row["name"]." ".$row["vorname"],$row["name"]." ".$row["vorname"],$row['name'][0].$row['vorname'][0].$row['verg_ID'],"2wsx","Schueler",$row["verg_ID"],$row["regelklasse"],preg_replace('/[^a-zA-Z]+/', '', $row["id"]);$row["id"],"Aarauerstrasse 30","4600","Olten","","Schweiz","CH",$row["telefon"],"","","",$row["mobile"],"","","","","","","","",$row["emailadresse"],$row['name'][0].$row['vorname'][0].$row['verg_ID']."@bbzolten.ch","","www.bbzolten.so.ch","","","1","1","1","240","","","1","0","","","1","1","1","Users","G_netDomain","G_Win7Office13","Schueler","G_WLanLuS","G_WLanBBZO","","","","","","","","","","","","","","","","","","","","","","","","");
+      $line = array("","",$row["name"],"",$row["vorname"],$row["name"]." ".$row["vorname"],$row["name"]." ".$row["vorname"],$row['name'][0].$row['vorname'][0].$row['verg_ID'],"2wsx","Schueler",$row["verg_ID"],$row["regelklasse"],preg_replace('/[^a-zA-Z]+/', '', $row["id"]),$row["id"],"Aarauerstrasse 30","4600","Olten","","Schweiz","CH",$row["telefon"],"","","",$row["mobile"],"","","","","","","","",$row["emailadresse"],$row['name'][0].$row['vorname'][0].$row['verg_ID']."@bbzolten.ch","","www.bbzolten.so.ch","","","1","1","1","240","","","1","0","","","1","1","1","Users","G_netDomain","G_Win7Office13","Schueler","G_WLanBBZO","","","","","","","","","","","","","","","","","","","","","","","","","");
       fputcsv($output, $line, ";");
       }
 
